ca2f2eae39d53aadfe6962ba911c837815bcba0b
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2023-01-23 16:25:27 +0300

    chore: rename plugin in tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import assert from 'assert';
 import { factory, runTasks } from 'release-it/test/util/index.js';
 import Plugin from './index.js';
 
-const namespace = 'my-plugin';
+const namespace = '@lightness/release-it-slack-notification-plugin';
 
 test('should not throw', async () => {
   const options = { [namespace]: {} };
